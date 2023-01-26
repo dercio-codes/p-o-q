f2c0372f3a383b9bd24e5c66f456909c9ee3473c
Dercio Maduna <12derciomaduna@gmail.com>
2023-01-26 00:01:30 +0000

    Update index.jsx

--- components/profile/index.jsx
+++ components/profile/index.jsx
@@ -341,12 +341,16 @@ export const ProfileComponent = () => {
               left: "0",
               zIndex: "99999999",
               display: openStories ? "flex" : "none",
-              alignItems: "center",
+              // alignItems: "flex-end",
               justifyContent: "center",
+              flexDirection: "column",
             }}
             onClick={() => setOpenProfileModal(false)}
-          > 
-          <ArrowBackIcon             onClick={() => setOpenProfileModal(false)}/>
+          >
+            <ArrowBackIcon
+              sx={{ margin: "21px"  }}
+              onClick={() => setOp(false)}
+            />
             <Stories />
           </Box>
           <Dialog
